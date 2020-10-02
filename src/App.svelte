40c47c7465a94dd917c9f6904b89fdb856b934f1
Simon Mudd <smudd@amplience.com>
2020-10-02 14:53:38 +0100

    feat(aotw form): added basic aotw form component

---
+++ src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+	import 'bulma/css/bulma.css'
+	import Form from './components/Form.svelte'
+	export let name;
+
+	let user = { submitForm: false };
+
+	function submit() {
+		user.submitForm = !user.submitForm;
+	}
+</script>
+
+<main>
+	{#if !user.submitForm}
+		<section class="hero is-info">
+			<div class="hero-body">
+			<div class="container">
+				<h1 class="title">
+					Hello {name}!
+				</h1>
+				<h2 class="subtitle">
+				Submit your Album of the week.
+				</h2>
+			</div>
+			</div>
+		</section>
+		<Form />
+	{/if}
+	{#if user.submitForm}
+		<section class="hero is-info">
+			<div class="hero-body">
+			<div class="container">
+				<h1 class="title">
+					Thank you {name}!
+				</h1>
+				<h2 class="subtitle">
+				Keep on rocking.
+				</h2>
+			</div>
+			</div>
+		</section>
+	{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		/* padding: 1em; */
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #2980b9;
+		text-transform: uppercase;
+		font-size: 3em;
+		font-weight: 100;
+		font-family: sans-serif; 
+	}
+
+	.hero {
+		margin-bottom: 20px;
+	}
+</style>
---
+++ src/components/Form.svelte
@@ -0,0 +1,22 @@
+<div class="container is-fluid">
+    <div class="columns">
+        <div class="column is-4 is-offset-4">
+            <div class="field">
+                <div class="control">
+                  <input class="input is-medium" type="text" placeholder="Artist">
+                </div>
+            </div>
+            <div class="field">
+                <div class="control">
+                  <input class="input is-medium" type="text" placeholder="Album title">
+                </div>
+            </div>
+            <div class="field">
+                <div class="control">
+                  <input class="input is-medium" type="text" placeholder="Link (Spotify, YouTube, etc)">
+                </div>
+            </div>
+            <button class="button is-info is-medium is-fullwidth is-uppercase">Submit album</button>
+        </div>
+    </div>
+</div>
